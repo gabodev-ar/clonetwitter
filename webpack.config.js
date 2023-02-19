8f3c7acd277f29508fff1f87b7266713368ee568
GaboDev <gonzaloramirez.sagitario@gmail.com>
2023-02-18 22:42:13 -0300

    Finalizado el navbar version desktop

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   //Punto de entrada, punto de salida
@@ -21,6 +22,15 @@ module.exports = {
 
     new MiniCssExtractPlugin({
       filename: "style.css"
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/img"),
+          to: "assets/img"
+        }
+      ]
     })
   ],
 
