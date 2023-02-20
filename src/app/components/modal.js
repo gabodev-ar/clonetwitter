176352b406b9ff3cc11e1569803246e6b9927ef7
GaboDev <gonzaloramirez.sagitario@gmail.com>
2023-02-20 17:49:48 -0300

    Avances sobre el componente modal

--- src/app/components/modal.js
+++ src/app/components/modal.js
@@ -1,14 +1,25 @@
-const textArea = document.getElementById('input-tweet');
-const modal    = document.querySelector('.modal');
+const textArea    = document.getElementById('input-tweet');
+const modal       = document.querySelector('.modal');
+const modalHeader = document.querySelector('.modal__header');
 
-
-
-
-// console.log(textArea.rows);
-
+//Auto-resize input Tweet
 const autoResize = ()=>{
     textArea.style.height = 0;
-    textArea.style.height = `${textArea.scrollHeight}px`
-}
+    textArea.style.height = `${textArea.scrollHeight}px`;
+};
 
 textArea.addEventListener('input', autoResize);
+
+//Scroll Background Header
+
+const scrollBackground = ()=>{
+
+    console.log(modal.scrollTop);
+
+    (modal.scrollTop > 0) ? 
+        modalHeader.classList.add('modal__header--active') :
+        modalHeader.classList.remove('modal__header--active');
+
+};
+
+modal.addEventListener('scroll', scrollBackground); 
