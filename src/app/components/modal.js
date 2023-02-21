5de352aa9e0035b6550adffcd2c79e1b8f472695
GaboDev <gonzaloramirez.sagitario@gmail.com>
2023-02-20 21:17:27 -0300

    Finalizada la maquetación del componente modal

--- src/app/components/modal.js
+++ src/app/components/modal.js
@@ -1,6 +1,10 @@
-const textArea    = document.getElementById('input-tweet');
-const modal       = document.querySelector('.modal');
-const modalHeader = document.querySelector('.modal__header');
+const textArea    = document.getElementById('input-tweet'),
+      modal       = document.querySelector('.modal'),
+      modalCover  = document.getElementById('modalCover'),
+      modalHeader = document.querySelector('.modal__header'),
+      modalClose  = document.getElementById('modalClose'),
+      twittearBtn = document.getElementById('twittearBtn');
+
 
 //Auto-resize input Tweet
 const autoResize = ()=>{
@@ -11,15 +15,32 @@ const autoResize = ()=>{
 textArea.addEventListener('input', autoResize);
 
 //Scroll Background Header
-
 const scrollBackground = ()=>{
 
     console.log(modal.scrollTop);
 
-    (modal.scrollTop > 0) ? 
-        modalHeader.classList.add('modal__header--active') :
+    if(modal.scrollTop > 0){
+        modalHeader.classList.add('modal__header--active');
+    }else {
         modalHeader.classList.remove('modal__header--active');
+    };
 
 };
 
 modal.addEventListener('scroll', scrollBackground); 
+
+//Action button Twittear
+const showModal = (e)=>{
+    e.preventDefault();
+    modalCover.classList.add('cover--active');
+    modal.classList.add('modal--active')
+}
+
+const hiddenModal = ()=>{
+    modalCover.classList.remove('cover--active');
+    modal.classList.remove('modal--active');
+}
+
+twittearBtn.addEventListener('click', showModal);
+modalClose.addEventListener('click', hiddenModal);
+modalCover.addEventListener('click', hiddenModal);
