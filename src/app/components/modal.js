7ed67a059463f5cebf5c2b03f7dbe510a48d7303
GaboDev <gonzaloramirez.sagitario@gmail.com>
2023-02-21 22:21:33 -0300

    Finalizado el componente spinner de progreso para la ventana mmodal

--- src/app/components/modal.js
+++ src/app/components/modal.js
@@ -1,10 +1,13 @@
-const textArea    = document.getElementById('input-tweet'),
-      modal       = document.querySelector('.modal'),
-      modalCover  = document.getElementById('modalCover'),
-      modalHeader = document.querySelector('.modal__header'),
-      modalClose  = document.getElementById('modalClose'),
-      twittearBtn = document.getElementById('twittearBtn');
-
+const textArea   = document.getElementById('inputTweet'),
+    modal        = document.getElementById('modal'),
+    modalCover   = document.getElementById('modalCover'),
+    modalHeader  = document.getElementById('modalHeader'),
+    modalClose   = document.getElementById('modalClose'),
+    modalCircle  = document.getElementById('modalCircleCheck'),
+    modalButton  = document.getElementById('modalButton'),
+    modalWarning = document.getElementById('modalWarning'),
+    twittearBtn  = document.getElementById('twittearBtn');
+    body         = document.querySelector('.body');
 
 //Auto-resize input Tweet
 const autoResize = ()=>{
@@ -16,8 +19,6 @@ textArea.addEventListener('input', autoResize);
 
 //Scroll Background Header
 const scrollBackground = ()=>{
-
-    console.log(modal.scrollTop);
 
     if(modal.scrollTop > 0){
         modalHeader.classList.add('modal__header--active');
@@ -33,14 +34,55 @@ modal.addEventListener('scroll', scrollBackground);
 const showModal = (e)=>{
     e.preventDefault();
     modalCover.classList.add('cover--active');
-    modal.classList.add('modal--active')
+    modal.classList.add('modal--active');
+    body.style = 'overflow : hidden';
 }
 
 const hiddenModal = ()=>{
     modalCover.classList.remove('cover--active');
     modal.classList.remove('modal--active');
+    body.style = 'overflow : auto';
 }
 
 twittearBtn.addEventListener('click', showModal);
 modalClose.addEventListener('click', hiddenModal);
 modalCover.addEventListener('click', hiddenModal);
+
+
+//Spinner Function
+
+const updateCircleCheck = ()=>{
+
+    let characters = textArea.value.length;
+    let pathlength = characters * 100 / 280; 
+    modalCircle.style = `stroke-dasharray: ${pathlength} 100;`;
+
+    if(characters > 259){
+        modalCircle.style.stroke = "#f9e793";
+        modalWarning.style.display = "block";
+        modalWarning.innerText = `${280 - characters}`;
+        
+    }
+    
+    if(characters > 280){
+        modalCircle.style.stroke = "#bc333e";
+        modalWarning.style.color = "#bc333e";
+    }
+
+    (characters > 0) ? modalButton.disabled=false : modalButton.disabled=true;
+
+    console.log(characters);
+
+}
+
+textArea.addEventListener('input', updateCircleCheck);
+
+
+modalButton.addEventListener('click', ()=>{
+    console.log('OK')
+})
+
+
+
+
+
